5caaeaca8c06d4f3cc24723157b2a7f136cc22a2
kivas1k <kivis006@gmail.com>
2024-10-23 21:22:44 +0700

    first commit

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, register_converter
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),  # Главная страница
+    path('about/', views.about, name='about'),  # о нас
+    path('cat/<int:cat_id>', views.categories, name='cat_id'),  # Каталог
+    path('cat/<slug:cat_slug>', views.categories_by_slug, name='cat'),  # Каталог
+]
+
+
---
+++ main/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.template.loader import render_to_string
+
+from django.shortcuts import render
+
+menu = ["О нас", "Каталог", "Войти"]
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+    }
+    return render(request, 'main/index.html', context=data)
+
+def about(request):
+    return render(request, 'main/about.html', {'title': 'О сайте', 'menu': menu})
+
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>тут будет каталог</h1><p>id: {cat_id}</p>")
+
+def categories_by_slug(request, cat_slug):
+    if request.POST:
+        print(request.POST)
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>нету тут ниче</h1>")
+
