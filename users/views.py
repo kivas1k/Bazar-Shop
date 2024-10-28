7ee225176b272de188435d9461a885db1638b060
kivas1k <kivis006@gmail.com>
2024-10-28 20:44:44 +0700

    fix html +add html

--- users/views.py
+++ users/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from .forms import UserRegistrationForm
+
+menu = ["О нас", "Каталог", "Войти"]  # Общая переменная для меню
 
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # автоматически выполняем вход после регистрации
+            login(request, user)  # автоматический вход после регистрации
             return redirect('home')  # перенаправляем на главную страницу
     else:
         form = UserRegistrationForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'users/register.html', {'form': form, 'menu': menu})
 
 def profile_view(request):
-       return render(request, 'main/profile.html', {'title': 'Профиль', 'menu': menu})
+    if not request.user.is_authenticated:
+        return redirect('login')
+    return render(request, 'users/profile.html', {'title': 'Профиль', 'menu': menu})
 
 def login_view(request):
     if request.method == 'POST':
@@ -25,5 +29,14 @@ def login_view(request):
             login(request, user)
             return redirect('home')
         else:
-            return render(request, 'users/login.html', {'error': 'Неверное имя пользователя или пароль.'})
-    return render(request, 'users/login.html')
+            return render(request, 'users/login.html', {'error': 'Неверное имя пользователя или пароль.', 'menu': menu})
+    return render(request, 'users/login.html', {'menu': menu})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+def profile_edit_view(request):
+
+    return render(request, 'users/profile_edit.html', {'title': 'Редактирование профиля'})
