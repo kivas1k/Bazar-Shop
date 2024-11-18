d773a0fef6bd50547ae8fab65832b5200e108411
kivas1k <kivis006@gmail.com>
2024-11-18 10:58:12 +0700

    fix login and register for admin

--- users/backends.py
+++ users/backends.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = User.objects.get(email=username)
+            user = User.objects.get(email=username)  # Попытка найти пользователя по email
         except User.DoesNotExist:
-            return None
+            try:
+                user = User.objects.get(username=username)  # Попытка найти пользователя по username
+            except User.DoesNotExist:
+                return None
         if user.check_password(password):
             return user
         return None
--- users/forms.py
+++ users/forms.py
@@ -5,21 +5,16 @@ from .models import UserProfile
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True, label="Email")
-    avatar = forms.ImageField(required=False, label="Аватар", widget=forms.ClearableFileInput(attrs={'multiple': False}))
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'avatar']
+        fields = ['username', 'email', 'password1', 'password2']  # Убираем avatar
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
-            if self.cleaned_data.get('avatar'):
-                user_profile, created = UserProfile.objects.get_or_create(user=user)
-                user_profile.avatar = self.cleaned_data['avatar']
-                user_profile.save()
         return user
 
 class UserProfileForm(forms.ModelForm):
@@ -28,4 +23,14 @@ class UserProfileForm(forms.ModelForm):
         fields = ['bio', 'avatar']
 
 class EmailAuthenticationForm(AuthenticationForm):
-    username = forms.EmailField(label="Email")  # Заменяем поле на email
+    username = forms.CharField(
+        label="Email или Имя пользователя",
+        required=True,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Введите email или Имя пользователя',
+        })
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].initial = ''
